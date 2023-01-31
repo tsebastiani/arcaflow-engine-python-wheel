b5a97571b1c710cfe828d4f46947fadb26028377
Tullio Sebastiani <tullio.sebastiani@x3solutions.it>
2023-01-31 18:55:33 +0100

    restored version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='arcaflowengine',
-    version='0.1.1',
+    version='1.0.0',
     license_files = ('LICENSE',),
     package_data={
         'arcaflowengine':['bin/arcaflow*']
